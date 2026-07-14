871119e6a41c9d985a64967c84d0ad545111f474
rijanbht <rijanbht@gmail.com>
2026-07-14 12:25:39 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route}from 'react-router-dom';
 import Login from './Components/Assets/LoginSignUp/LoginSignup';
 import Dashboard from './Components/Dashboard/Dashboard';
+import TaskList from './Components/TaskList/TaskList';
 
 function App() { //paths for links instead of hrefs
   return (
@@ -8,6 +9,7 @@ function App() { //paths for links instead of hrefs
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/tasks" element={<TaskList />} />
       </Routes>
     </BrowserRouter>
   );
